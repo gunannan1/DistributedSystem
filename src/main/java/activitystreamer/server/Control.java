7f50ff71817c3adce15495979c729db0c9533ff0
Nannan Gu <396426364@qq.com>
2018-04-16 09:14:29 +1000

    supply authfail

--- src/main/java/activitystreamer/server/Control.java
+++ src/main/java/activitystreamer/server/Control.java
@@ -179,7 +179,7 @@ public class Control extends Thread {
 		log.debug("outgoing connection: " + Settings.socketAddress(s));
 		Connection c = new Connection(s);
 		connections.add(c);
-		c.sendAuthMsg(Settings.getSecret());
+//		c.sendAuthMsg(Settings.getSecret());
 		return c;
 
 	}
